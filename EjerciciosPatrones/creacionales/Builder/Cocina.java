4d4ae3b67790e346aa3509ce8b0e57951943a37e
tomasfelder <tomas.felder@gmail.com>
2017-03-14 17:09:46 -0300

    Quedo clase sin guardar

--- EjerciciosPatrones/creacionales/Builder/Cocina.java
+++ EjerciciosPatrones/creacionales/Builder/Cocina.java
@@ -1,5 +1,20 @@
 package Builder;
 
 public class Cocina {
-
+	private PizzaBuilder pizzaBuilder;
+	
+	public void setPizzaBuilder(PizzaBuilder pz){
+		pizzaBuilder=pz;
+	}
+	
+	public Pizza getPizza(){
+		return pizzaBuilder.getPizza();
+	}
+	
+	public void construirPizza(){
+		pizzaBuilder.crearNuevaPizza();
+		pizzaBuilder.buildMasa();
+		pizzaBuilder.buildSalsa();
+		pizzaBuilder.buildRelleno();
+	}
 }
